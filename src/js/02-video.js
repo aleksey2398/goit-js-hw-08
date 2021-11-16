8bcf8348258eee9d79bc6eee116aa7f8fee88ddc
aleksey2398 <87311512+aleksey2398@users.noreply.github.com>
2021-11-16 20:05:38 +0200

    next

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-const Player_Video = 'videoplayer-current-time';
+const PLAYER_VIDEO = 'videoplayer-current-time';
 const onPlay = function (data) {
   const time = JSON.stringify(data.seconds);
-  localStorage.setItem(Player_Video, time);
+  localStorage.setItem(PLAYER_VIDEO, time);
   console.log('played the video!', time);
 };
 player.on('timeupdate', throttle(onPlay, 1000));
-const saveTime = localStorage.getItem(Player_Video);
+const saveTime = localStorage.getItem(PLAYER_VIDEO);
 player
   .setCurrentTime(saveTime)
   .then(function (seconds) {
