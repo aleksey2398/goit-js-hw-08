0c2ff0ca5c56262cca7d94e22a2d0ea479b49553
aleksey2398 <87311512+aleksey2398@users.noreply.github.com>
2021-11-17 13:41:41 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,29 +5,35 @@ const refs = {
   input: document.querySelector('.feedback-form input'),
 };
 const STORAGE_KEY = 'feedba0ck-form-state';
-populateTextarea();
+
 let formData = {};
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
 function onFormSubmit(evt) {
   evt.preventDefault();
-  console.log(formData);
-  formData = {};
+  console.log(`${refs.input.name}:`, refs.input.value);
+  console.log(`${refs.textarea.name}:`, refs.textarea.value);
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
+
 function onTextareaInput(evt) {
-  formData[evt.target.name] = evt.target.value;
+  formData[refs.textarea.name] = refs.textarea.value;
+  formData[refs.input.name] = refs.input.value;
   const saveDataEl = JSON.stringify(formData);
   localStorage.setItem(STORAGE_KEY, saveDataEl);
 }
+
+const savedMessage = localStorage.getItem(STORAGE_KEY);
+const pasrsedSav = JSON.parse(savedMessage);
+
 function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
-    const pasrsedSav = JSON.parse(savedMessage);
     const keys = Object.keys(pasrsedSav);
     for (const key of keys) {
       refs.form.elements[key].value = pasrsedSav[key];
     }
   }
 }
+populateTextarea();
